f22511c58b1bb414b04fcfcd196d9ae187b4fd28
Ptr-k <pcamvor611@g.educaand.es>
2025-11-25 10:16:07 +0100

    Added ServletInitializer.java to EmpresaSpringApplication.java

--- src/main/java/com/empresaSpring/EmpresaSpringApplication.java
+++ src/main/java/com/empresaSpring/EmpresaSpringApplication.java
@@ -2,9 +2,16 @@ package com.empresaSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmpresaSpringApplication {
+public class EmpresaSpringApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmpresaSpringApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpresaSpringApplication.class, args);
